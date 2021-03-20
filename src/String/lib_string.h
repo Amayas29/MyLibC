ff63c254030df954863857a819c25972030ebc2d
Amayas <65504644+Amayas29@users.noreply.github.com>
2021-03-20 21:44:49 +0100

    Update lib_string.h

--- src/String/lib_string.h
+++ src/String/lib_string.h
@@ -1,11 +1,6 @@
 #ifndef __LIB_STRING_H__
 #define __LIB_STRING_H__
 
-/*
-* Version 01: using linked list
-* Version 02: with realloc
-*/
-
 /* Split a string according to the delimiter */
 char **split_l(const char str[], const char delim[], int *length); // Using linked list
 char **split_r(const char str[], const char delim[], int *length); // Using realloc
