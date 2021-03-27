df91ba76f2c95956227ff5a41700679b0ce0d6e4
Amayas29 <amayas29sd@gmail.com>
2021-03-27 12:00:54 +0100

    Prepare the struct

--- src/error/error.h
+++ src/error/error.h
@@ -3,4 +3,12 @@
 
 #include <stdio.h>
 
+typedef enum {
+    TMP
+} error_t;
+
+const char *ERROR_MESSAGES[] = {};
+
+void raise(error_t err, const char *FILE, const char *FUNCTION, const int LINE, char *details);
+
 #endif
