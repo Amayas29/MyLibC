c4905c0ac6bf3cf2a57acbc4cb1e1daa14638858
Amayas Sadi <65504644+Amayas29@users.noreply.github.com>
2021-04-09 19:35:22 +0200

    Update lib_error.c

--- src/error/lib_error.c
+++ src/error/lib_error.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 static const char *ERROR_MESSAGES[] = {
-    "NullPointerError : The pointer is NULL",
+    "NullPointerError : a pointer is NULL",
     "AllocationError : an allocation error has occurred",
-    "IndexOutOfBoundsError : acces to an out of bounds index"};
+    "IndexOutOfBoundsError : access to an out of bounds index"};
 
 void raise_error(error_t err, const char *FILE, const char *FUNCTION, const int LINE, char *details) {
     fprintf(stderr, "%s in %s -- %s at ligne %d.\n", ERROR_MESSAGES[err], FILE, FUNCTION, LINE);
