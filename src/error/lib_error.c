b27a2565fc358b5547481ff823961e3a37c10e88
Amayas29 <amayas29sd@gmail.com>
2021-04-18 17:31:40 +0200

    Add ValueError

--- src/error/lib_error.c
+++ src/error/lib_error.c
@@ -5,7 +5,9 @@
 static const char *ERROR_MESSAGES[] = {
     "NullPointerError : a pointer is NULL",
     "AllocationError : an allocation error has occurred",
-    "IndexOutOfBoundsError : access to an out of bounds index"};
+    "IndexOutOfBoundsError : access to an out of bounds index",
+    "ValueError : the value is not valid",
+};
 
 void raise_error(error_t err, const char *FILE, const char *FUNCTION, const int LINE, char *details) {
     fprintf(stderr, "%s in %s -- %s at ligne %d.\n", ERROR_MESSAGES[err], FILE, FUNCTION, LINE);
--- src/error/lib_error.h
+++ src/error/lib_error.h
@@ -7,6 +7,7 @@ typedef enum {
     NullPointerError,
     AllocationError,
     IndexOutOfBoundsError,
+    ValueError,
 } error_t;
 
 void raise_error(error_t err, const char *FILE, const char *FUNCTION, const int LINE, char *details);
